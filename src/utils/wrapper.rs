b26459b19cb7f745aed59ad756ee555406d47658
ThePixelCode <thepixelcode@proton.me>
2025-05-01 04:58:02 -0500

    wrapper: Fixed errors

--- src/utils/wrapper.rs
+++ src/utils/wrapper.rs
@@ -1,6 +1,6 @@
 pub struct W<T>(pub T);
 
-pub trait Wrap {
+pub trait Wrap: Sized {
     fn wrap(self) -> W<Self>;
     fn unwrap(value: W<Self>) -> Self;
 }
@@ -15,14 +15,14 @@ impl<T: Sized> Wrap for T {
     }
 }
 
-impl<T> std::ops::Deref for T {
+impl<T> std::ops::Deref for W<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         &self.0
     }
 }
 
-impl<T: Debug> Debug for W<T> {
+impl<T: std::fmt::Debug> std::fmt::Debug for W<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.0.fmt(f)
     }
